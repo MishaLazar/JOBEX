a9b2eab6d8cd67da076e8674c23be01cad6a6669
nir.ostrovski <nir.ostrovski@tufin.com>
2019-01-01 16:12:58 +0200

    JOBEX-REST:    - updated web controller with new DB handler jobex-web-app:    - nothing

--- JOBEX-REST/Controllers/web_controller.py
+++ JOBEX-REST/Controllers/web_controller.py
@@ -1,5 +1,10 @@
-from DAL import mongo_db_handler
-import configparser
+from DAL.mongo_db_handler import Client
+
+COLLECTION_NAMES = {
+    "engagements_collection": "engagements",
+    "positions_collection": "positions",
+    "users_collection": "users"
+}
 
 class WebController:
 
@@ -19,34 +24,39 @@ class WebController:
         else:
             WebController.__instance = self
 
-    def status(self):
-        db = mongo_db_handler.WebDbHandler.getInstance()
-        return db.status()
-
     def add_engagement(self, engagement_obj):
-        db = mongo_db_handler.WebDbHandler.getInstance()
-        result = db.add_engagement(engagement_obj)
-        return result
+        db_client = Client()
+        return db_client.insert_doc_to_collection(COLLECTION_NAMES["engagements_collection"], engagement_obj)
 
     def get_engagements(self, company_name=None, engagement_id=None):
-        db = mongo_db_handler.WebDbHandler.getInstance()
+        db_client = Client()
         if company_name:
-            return db.get_all_engagements(company_name)
+            return db_client.get_many_docs_from_collection(COLLECTION_NAMES["engagements_collection"], {"company_name": company_name})
         elif company_name and engagement_id:
-            return db.get_engagement(company_name, engagement_id)
+            return db_client.get_single_doc_from_collection(COLLECTION_NAMES["engagements_collection"],
+                                                            {"_id": engagement_id, "company_name": company_name})
         else:
             return None
 
     def add_position(self, position_obj):
-        db = mongo_db_handler.WebDbHandler.getInstance()
-        result = db.add_position(position_obj)
-        return result
+        db_client = Client()
+        return db_client.insert_doc_to_collection(COLLECTION_NAMES["positions_collection"], position_obj)
 
     def get_positions(self, company_name=None, position_id=None):
-        db = mongo_db_handler.WebDbHandler.getInstance()
+        db_client = Client()
         if company_name:
-            return db.get_all_positions(company_name)
+            return db_client.get_many_docs_from_collection(COLLECTION_NAMES["positions_collection"], {"company_name": company_name})
         elif company_name and position_id:
-            return db.get_position(company_name, position_id)
+            return db_client.get_single_doc_from_collection(COLLECTION_NAMES["positions_collection"],
+                                                            {"company_name": company_name, "_id": position_id})
         else:
             return None
+
+    def get_user(self, user_id):
+        db_client = Client()
+        return db_client.get_single_doc_from_collection(COLLECTION_NAMES["users_collection"], {"_id": user_id})
+
+    def add_user(self, user_obj):
+        db_client = Client()
+        return db_client.insert_doc_to_collection(COLLECTION_NAMES["users_collection"], user_obj)
+
