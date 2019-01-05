9773bfd4e6429d311500f962a46dc5c4259d9d00
Misha Lazar <mishal@mail.afeka.ac.il>
2019-01-05 23:56:31 +0200

    1) login with localstorage keeping the tokens 2) navigation and user authentication state

--- student_jobex/src/app/app.component.ts
+++ student_jobex/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import {Component, ViewChild} from '@angular/core';
-import {MenuController, NavController, Platform} from 'ionic-angular';
+import {LoadingController, MenuController, NavController, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { HomePage } from '../pages/home/home';
 import {LoginPage} from "../pages/login/login";
 import {RegisterPage} from "../pages/register/register";
+
 
 @Component({
   templateUrl: 'app.html'
@@ -14,21 +14,49 @@ export class MyApp {
   homePage = HomePage;
   loginPage = LoginPage;
   registerPage = RegisterPage;
+  isAuthenticated:boolean;
 
   @ViewChild('nav') nav: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+              private menuCtrl: MenuController , private loaderCtrl: LoadingController) {
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.onAuthenticationCheck();
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
   }
 
   onLoad(page:any){
     this.nav.setRoot(page);
     this.menuCtrl.close();
   }
+
+  onAuthenticationCheck(){
+    const loader = this.loaderCtrl.create({
+      content:'Loading...'
+    });
+    loader.present();
+    //let t = setTimeout(() => {
+      if (localStorage.getItem('access_token') != null && localStorage.getItem('refresh_token') != null) {
+        console.log('authenticated');
+        this.isAuthenticated = true;
+        this.nav.setRoot(this.homePage);
+      }
+      else {
+        console.log('not authenticated');
+        //this.nav.setRoot(this.loginPage);
+        this.isAuthenticated = false;
+      }
+
+      loader.dismiss();
+    //},100);
+
+  }
+
 }
 
--- student_jobex/src/services/authentication.service.ts
+++ student_jobex/src/services/authentication.service.ts
@@ -1,14 +1,17 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {catchError} from "rxjs/operators";
+import {StorageService} from "./storage.service";
+
 
 
 @Injectable()
 export class AuthenticationService {
 
-  constructor(private http: HttpClient){}
-
   api_url = 'http://127.0.0.1:5050/';
+  isAuthenticatedStatus:boolean = false;
+
+  constructor(private http: HttpClient,private storageCtrl:StorageService){}
+
 
   onSignin(username:string, password:string){
 
@@ -24,4 +27,15 @@ export class AuthenticationService {
 
   }
 
+  onLogout(){
+    this.storageCtrl.removeItemByKey('access_token');
+    this.storageCtrl.removeItemByKey('refresh_token');
+  }
+
+  isAuthenticated() {
+    console.log('isAuthenticated :' + this.storageCtrl.getValueByKey('token'));
+    return this.storageCtrl.getValueByKey('token') !== null;
+
+
+  }
 }
