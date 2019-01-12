aca367456e8257ca28603988b234be66774ddced
Misha Lazar <mishal@mail.afeka.ac.il>
2019-01-12 13:19:04 +0200

    static dbcollection names get functions fetch data about positions and students skills update job status fixed

--- JOB-Service/jobs_service.py
+++ JOB-Service/jobs_service.py
@@ -2,10 +2,10 @@ import json
 import threading
 import time
 import logging
-
+from bson import ObjectId
 from Classes.job import Job
-from config_helper import ConfigHelper
-from mongo_db_handler import Client
+from Utils.config_helper import ConfigHelper
+from DAL.mongo_db_handler import Client
 from DAL.db_collections import DbCollections
 from Utils.json_util import JSONEncoder
 
@@ -54,8 +54,11 @@ class JobThread(threading.Thread):
 
         for j in jobs:
             json_obj = json.loads(JSONEncoder().encode(j))
-            job = Job(job_id=json_obj['_id'], job_type_id=int(json_obj['job_type_id']), source_objectid=json_obj['source_objectid'],
-                      creation_date=json_obj['creation_date'], status=int(json_obj['status']))
+            job = Job(job_id=json_obj['_id'],
+                      job_type_id=int(json_obj['job_type_id']),
+                      source_objectid=json_obj['source_objectid'],
+                      creation_date=json_obj['creation_date'],
+                      status=int(json_obj['status']))
             self.logger.debug('job working on:' + job.job_id)
             if int(job.job_type_id) == 1:  # rematch by student
                 self.logger.debug('rematch by student_id :' + job.source_objectid)
@@ -76,14 +79,14 @@ class JobThread(threading.Thread):
         query = {
             "status": 0
         }
-        return db_client.get_many_docs_from_collection(DbCollections.get_collection('jobs'), query)
+        return db_client.get_many_docs_from_collection(DbCollections.get_job_collection(), query)
 
     def get_all_active_positions(self):
         db_client = Client()
         query = {
             "active": "Yes"
         }
-        return db_client.get_many_docs_from_collection(DbCollections.get_collection('positions_col'), query)
+        return db_client.get_many_docs_from_collection(DbCollections.get_position_collection(), query)
 
     def get_all_active_students(self):
         db_client = Client()
@@ -91,15 +94,37 @@ class JobThread(threading.Thread):
             "UserTypeId": 1,
             "status":1
         }
-        return db_client.get_many_docs_from_collection(DbCollections.get_collection('users_col'), query)
+        return db_client.get_many_docs_from_collection(DbCollections.get_student_collection(), query)
 
     def on_job_finish(self, job, status):
         db_client = Client()
-        filter_json = {"_id": job.job_id}
+        filter_json = {"_id": ObjectId(job.job_id)}
         doc_json = {'$set': {'status': status}}
 
         return db_client.update_single_doc_in_collection(DbCollections.get_collection('jobs'),
                                                          filter_json=filter_json, doc_update_json=doc_json)
+
+    def on_get_student_skills(self,_id):
+        db_client = Client()
+        query = {
+            "student_id": _id
+        }
+        return db_client.get_many_docs_from_collection(DbCollections.get_student_skills_collection(), query)
+
+    def on_get_position_skills(self, _id):
+        db_client = Client()
+        query = {
+            "position_id": _id
+        }
+        return db_client.get_many_docs_from_collection(DbCollections.get_position_skills_collection(), query)
+
+    def update_match_level(self,student_id, position_id, match_level):
+        db_client = Client()
+        query = {
+            "position_id": position_id,
+            "student_id": student_id
+        }
+        match = db_client.get_many_docs_from_collection(DbCollections.get_match_collectio(),json_query=query)
 
 
 log = logging.getLogger(name='Job - Service')
